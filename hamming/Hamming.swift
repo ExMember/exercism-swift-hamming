968ea88f8aa1c1ee31970d65bfcc082fe47df846
Damien Burke <damien@Feather.local>
2015-06-01 15:59:07 -0700

    Add passing implementation

---
+++ hamming/Hamming.swift
@@ -0,0 +1,26 @@
+//
+//  Hamming.swift
+//  hamming
+//
+//  Created by Damien Burke on 6/1/15.
+//  Copyright (c) 2015 Damien Burke. All rights reserved.
+//
+
+import Foundation
+
+class Hamming {
+    class func compute(initial:String, against:String) -> Int? {
+        if count(initial) != count(against) {
+            return nil
+        }
+        
+        var dist = 0
+        
+        for index in indices(initial) {
+            if initial[index] != against[index] {
+                dist++
+            }
+        }
+        return dist
+    }
+}
